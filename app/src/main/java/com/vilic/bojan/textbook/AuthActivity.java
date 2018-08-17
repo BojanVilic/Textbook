04edbdddd82e78d21f0ee319c14252b0aef232b7
Bojan <bojan.vilic@gmail.com>
2018-08-17 12:14:37 +0200

    Authentication done, navigation to be done

---
+++ app/src/main/java/com/vilic/bojan/textbook/AuthActivity.java
@@ -0,0 +1,144 @@
+package com.vilic.bojan.textbook;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.security.AccessController.getContext;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private CountryCodePicker mCodePicker;
+    private EditText mNumber, mCountryCode, mVerificationCode;
+    private Button mVerifyButton;
+    private String mPhoneNumber;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private ProgressDialog progressDialog;
+    int btnType = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mCodePicker = (CountryCodePicker) findViewById(R.id.ccp);
+        mNumber = (EditText) findViewById(R.id.number);
+        mCountryCode = (EditText) findViewById(R.id.countryCode);
+        mVerifyButton = (Button) findViewById(R.id.verifyButton);
+        mVerificationCode = (EditText) findViewById(R.id.code) ;
+
+        progressDialog = new ProgressDialog(this);
+
+        mCodePicker.getSelectedCountryCode();
+        mCountryCode.setText("+ " + mCodePicker.getSelectedCountryCode());
+
+        mCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                mCountryCode.setText("+ " + mCodePicker.getSelectedCountryCode());
+                mCountryCode.setVisibility(View.VISIBLE);
+                }
+        });
+
+        mVerifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btnType == 0) {
+
+                    progressDialog.setMessage("Verifying, please wait.");
+                    progressDialog.setCancelable(false);
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
+
+                    btnType = 1;
+
+                    mVerifyButton.setEnabled(false);
+                    mPhoneNumber = mCodePicker.getSelectedCountryCode() + mNumber.getText().toString();
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(mPhoneNumber, 60, TimeUnit.SECONDS, AuthActivity.this, mCallbacks);
+                }
+                else {
+                    mVerifyButton.setEnabled(false);
+
+                    String verificationCode = mVerificationCode.getText().toString();
+
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                    signInWithPhoneAuthCredential(credential);
+                }
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(AuthActivity.this, "There was some error. Please try again.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                mVerificationId = s;
+                mResendToken = forceResendingToken;
+
+                mCountryCode.setVisibility(View.INVISIBLE);
+                mNumber.setVisibility(View.INVISIBLE);
+                mVerificationCode.setVisibility(View.VISIBLE);
+                mVerificationCode.animate().translationY(-70).start();
+                mVerifyButton.setEnabled(true);
+                mVerifyButton.setText("Verify code");
+                if(progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+            }
+        };
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = task.getResult().getUser();
+                    Intent mainIntent = new Intent(AuthActivity.this, IntroProfileSetup.class);
+                    startActivity(mainIntent);
+                    finish();
+                } else {
+                    if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/vilic/bojan/textbook/IntroProfileSetup.java
@@ -0,0 +1,149 @@
+package com.vilic.bojan.textbook;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.futuremind.recyclerviewfastscroll.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class IntroProfileSetup extends AppCompatActivity {
+
+    private CircleImageView mProfileImage;
+    private EditText mUsersName;
+    private Button mNextButton;
+    private TextView mAppearingDescription;
+    private static final int GALLERY_CODE = 1;
+    private String uid;
+    private StorageReference mStorageReference;
+    private DatabaseReference mDatabaseRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_profile_setup);
+
+        mProfileImage = (CircleImageView) findViewById(R.id.profilImage);
+        mUsersName = (EditText) findViewById(R.id.usersName);
+        mAppearingDescription = (TextView) findViewById(R.id.textView4);
+        mNextButton = (Button) findViewById(R.id.nextButton);
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+
+
+        FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = current_user.getUid();
+
+        mDatabaseRoot = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery, "Select Image"), GALLERY_CODE);
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = mUsersName.getText().toString();
+                if(!TextUtils.isEmpty(userName)){
+                    mDatabaseRoot.child("display_name").setValue(userName);
+                    mNextButton.setEnabled(false);
+                    Intent intent = new Intent(IntroProfileSetup.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(IntroProfileSetup.this, "Enter your name.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_CODE && resultCode == RESULT_OK){
+            Uri imgUri = data.getData();
+            CropImage.activity(imgUri).setAspectRatio(1, 1).start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                final ProgressDialog progressDialog = new ProgressDialog(IntroProfileSetup.this);
+                progressDialog.setMessage("Uploading image...");
+                progressDialog.show();
+                Uri resultUri = result.getUri();
+                StorageReference filepath = mStorageReference.child("profile_images").child(uid + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            final String downloadUrl = task.getResult().getDownloadUrl().toString();
+                            mDatabaseRoot.setValue("Proba, jedan");
+                            mDatabaseRoot.child("image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Picasso.get().load(downloadUrl).into(mProfileImage);
+                                    }
+                                }
+                            });
+                            progressDialog.dismiss();
+                            mUsersName.setEnabled(true);
+                            mNextButton.setEnabled(true);
+                            mAppearingDescription.animate().alpha(1f).setDuration(500).start();
+                            mUsersName.animate().alpha(1f).setDuration(500).start();
+                            mNextButton.animate().alpha(1f).setDuration(500).start();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+        /*HashMap<String, String> userMap = new HashMap<>();
+        userMap.put("name", "Bojan");
+        userMap.put("profileImage", "profile image url");
+        mDatabaseRoot.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(IntroProfileSetup.this, "Working", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(IntroProfileSetup.this, "NOT Working", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+
+
+    }
+}
